d279af4360355bbc823e4e476bc8128350d462d3
Bhargavi_20 <bhargaviketha0@gmail.com>
2025-12-02 17:34:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let menuBtn = document.querySelector(".menu-btn");
+let navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.style.display =
+        navLinks.style.display === "flex" ? "none" : "flex";
+});
